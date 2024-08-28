0c9c3b4b841d2bd062e01d0fcc55d1743dd046e5
harshit gupta <7317500h.harsh@gmail.com>
2024-08-28 23:02:29 +0530

    Add more

---
+++ Other code/Binary search/Peek value/Main.java
@@ -0,0 +1,27 @@
+// find the peek value in the mauntain array
+public class Main
+{
+    static int PeekValue(int arr[]){
+        int ans = 0;
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            
+            if( arr[mid+1] < arr[mid]  && arr[mid] > arr[mid-1]){
+                ans = mid;
+                break;
+            }else if(arr[mid] < arr[mid+1]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+	public static void main(String[] args) {
+		int arr[] = {10,20,40,50,30};
+		int ans = PeekValue(arr);
+		System.out.println(ans);
+	}
+}
