a7f916e066d92050be366472f0ee2e09a19e87b5
harshit gupta <7317500h.harsh@gmail.com>
2024-08-28 23:55:10 +0530

    u

--- Other code/Binary search/Peek value/Main.java
+++ Other code/Binary search/Peek value/Main.java
@@ -19,9 +19,27 @@ public class Main
         }
         return ans;
     }
+    // optimize solution
+    static int newPeekValue(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+     
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+       
+        return start;
+    }
 	public static void main(String[] args) {
 		int arr[] = {10,20,40,50,30};
 		int ans = PeekValue(arr);
+		int ans2 = newPeekValue(arr);
 		System.out.println(ans);
+		System.out.println(ans2);
 	}
 }
